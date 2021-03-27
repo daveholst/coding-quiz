5175ff3ff1a2371e3b6c59780f8ef86bcc739671
Dave Holst <daveholst@gmail.com>
2021-03-27 11:45:44 +0800

    added id to question object structure

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+const questions = {
+  question1: {
+    question: 'Function and var are known as: ',
+    answers: ['Keywords','Data types','Declaration Statements','Prototypes'],
+    correctIndex: 2
+  },
+  question2: {
+    question: 'Function and var are known as: ',
+    answers: ['Keywords','Data types','Declaration Statements','Prototypes'],
+    correctIndex: 2
+  },
+}
+
+// function to generate 
+function startQuiz(qList) {
+  //individial variables for questions
+  const {question1, question2} = qList
+}
