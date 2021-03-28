f6a432455991dc4e4a62bb577ea2066760b5ad3b
Dave Holst <daveholst@gmail.com>
2021-03-28 15:22:16 +0800

    added more questions

--- assets/app.js
+++ assets/app.js
@@ -14,9 +14,27 @@ const questions = [
   },
   {
     id: 1,
-    question: 'SECOND QUESTION: ',
-    answers: ['Keywords','Data types','Declaration Statements','Prototypes'],
+    question: 'Which company developed JavaScript?',
+    answers: ['Netscape','Bell Labs','Sun Microsystems','IBM'],
+    correctIndex: 0
+  },
+  {
+    id: 2,
+    question: 'The _______ method of an Array object adds elements to the end of an array',
+    answers: ['.pop()','.shift()','.push()','.unshift()'],
     correctIndex: 2
+  },
+  {
+    id: 3,
+    question: 'What is the correct syntax for referring to an external script called "LFC.js"?',
+    answers: ['<script src="LFC.js">','<script source="LFC.js">','<script ref="LFC.js">','<script type="LFC.js">'],
+    correctIndex: 0
+  },
+  {
+    id: 4,
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answers: ['<meta>','<style>','<body>','<script>'],
+    correctIndex: 3
   }
 ]
 
